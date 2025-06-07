6e242df96e6fc700ffc79718a4a93e287fac7c24
jjwc3 <jjwjjw2007@naver.com>
2025-06-07 16:07:15 +0900

    250607

--- src/routes/Order.svelte
+++ src/routes/Order.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <!-- Full-Screen -->
-<div id="background" class="h-3/4 bg-gray-500">
+<div id="background-top" class="h-3/4 bg-gray-500">
     <div id="header-pay" class="h-1/4 bg-white content-center">
         <p class="text-center text-4xl">결제하기</p>
     </div>
@@ -23,20 +23,20 @@
 
     <div id="order-menu-p" class="flex flex-col m-2">
 <!--        order-menu-->
-        <div class="order_menu flex flex-row">
-            <div class="menu_img w-1/4 bg-gray-400 aspect-square align-middle content-center border-2 rounded-2xl mr-2">
+        <div class="order_menu flex-between">
+            <div class="menu_img aspect-square w-1/4 align-middle content-center border-2 rounded-2xl mr-2 bg-gray-400">
                 <img src={testg} alt="testg" class="w-auto">
             </div>
             <div class="menu_detail flex flex-col w-full pr-2">
                 <div class="menu_detail_top flex-between items-center h-6">
                     <p class="menu_name font-medium">asdf</p>
-                    <div class="flex items-center">
-                        <div class="flex flex-row mr-2 items-center">
-                            <button class="button_menu_count_minus flex items-center justify-center h-5 w-5 border-l-2 border-y-2 text-center bg-white rounded-l-lg">
+                    <div class="flex-items-center">
+                        <div class="flex-between mr-2 items-center">
+                            <button class="button_menu_count_minus h-5 w-5 p-1 border-l-2 border-y-2 rounded-l-lg flex-center text-center bg-white">
                                 <img src={iconMinus} alt="-">
                             </button>
-                            <div class="menu_count flex items-center justify-center h-5 w-5 border-2 text-center bg-white">1</div>
-                            <button class="button_menu_count_plus flex items-center justify-center h-5 w-5 border-r-2 border-y-2 text-center bg-white rounded-r-lg">
+                            <div class="menu_count h-5 w-5 p-1 border-2 flex-center text-center bg-white">1</div>
+                            <button class="button_menu_count_plus h-5 w-5 p-1 border-r-2 border-y-2 rounded-r-lg flex-center text-center bg-white">
                                 <img src={iconPlus} alt="+">
                             </button>
                         </div>
@@ -46,36 +46,44 @@
                     </div>
                 </div>
                 <hr class="my-1 border-gray-600">
-                <div class="flex flex-row justify-between">
+                <div class="flex-between">
                     <div class="menu-options text-sm">
-                        <div class="menu-option flex flex-row items-center">
+                        <div class="menu-option flex-items-center">
                             <img src={iconDot} alt="Dot" class="w-2 h-2 mr-2">
                             <p>음료: 콜라(R)</p>
                         </div>
-                        <div class="menu-option flex flex-row items-center">
+                        <div class="menu-option flex-items-center">
                             <img src={iconDot} alt="Dot" class="w-2 h-2 mr-2">
                             <p>사이드 메뉴: 후렌치 후라이(M)</p>
                         </div>
-                        <div class="menu-option flex flex-row items-center">
+                        <div class="menu-option flex-items-center">
                             <img src={iconDot} alt="Dot" class="w-2 h-2 mr-2">
                             <p>옵션 변경: 없음</p>
                         </div>
                     </div>
-                    <button class="menu-option-edit h-full bg-white border-2 rounded-xl p-2">수정하기</button>
+                    <button class="menu-option-edit h-full p-2 border-2 rounded-xl bg-white">수정하기</button>
                 </div>
 
 
             </div>
 
         </div>
-
     </div>
+</div>
 
-
+<div id="background-bottom" class="bg-red-200 h-1/4 flex flex-col">
+    <div id="order-price" class="text-2xl text-right p-2.5">결제 금액: 10,000원</div>
+    <hr>
+    <div class="flex flex-grow gap-4 p-4">
+        <button id="order-for-here" class="flex-grow h-full gap-1 border-2 rounded-3xl border-red-500">
+            <p class="font-bold text-3xl">매장식사</p>
+            <p>For Here</p>
+        </button>
+        <button id="order-to-go" class="flex-grow h-full gap-1 border-2 rounded-3xl border-blue-500">
+            <p class="font-bold text-3xl">포장주문</p>
+            <p>To Go</p>
+        </button>
     </div>
-
-<div class="bg-white h-1/4">
-
 </div>
 
 
